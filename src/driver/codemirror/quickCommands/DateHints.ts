0c8f0a2ee85d8fcc549f373f30ead772bf90b7cc
Lintao <Lintao.Amons@outlook.com>
2022-06-06 10:07:13 +0800

    fix: date format

--- src/driver/codemirror/quickCommands/DateHints.ts
+++ src/driver/codemirror/quickCommands/DateHints.ts
@@ -3,12 +3,12 @@ import dayjs from 'dayjs';
 
 const DateHints: Hint[] = [
   {
-    text: dayjs().format(dayjs().format("MM/DD/YYYY")),
+    text: dayjs().format("MM/DD/YYYY"),
     displayText: '/today',
     description: 'Today',
   },
   {
-    text: dayjs().format(dayjs().format("MM/DD/YYYY HH:mm:ss")),
+    text: dayjs().format("MM/DD/YYYY HH:mm:ss"),
     displayText: '/now',
     description: 'now',
   }
